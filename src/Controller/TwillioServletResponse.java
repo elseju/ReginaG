0db49d5c1c25ede28f46ed19e6e7350270aac212
Jaya Kasa <kasa288@gmail.com>
2016-04-17 02:27:44 -0400

    testing twillio

--- src/Controller/TwillioServletResponse.java
+++ src/Controller/TwillioServletResponse.java
@@ -1,5 +1,8 @@
 package Controller;
 
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -16,34 +19,18 @@ import com.twilio.sdk.verbs.Message;
 import static org.apache.http.HttpHeaders.USER_AGENT;
 
 public class TwillioServletResponse extends HttpServlet {
- 
     // service() responds to both GET and POST requests.
     // You can also use doGet() or doPost()
-    public void service(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
-        String sessionId = request.getRequestedSessionId();
+    @Override
+    public void service(ServletRequest request, ServletResponse response) throws ServletException, IOException {
+        super.service(request, response);
+        String serverName = request.getServerName();
         String fromNumber = request.getParameter("From");
         String body = request.getParameter("Body");
 
-        System.out.println("SessionID " + sessionId);
+        System.out.println("Server " + serverName);
         System.out.println("fromNumber " + fromNumber);
         System.out.println("body " + body);
-
-        String incoming = ReginaConnect.getInstance().getRequest(body,fromNumber);
-
-        /*
-        Message message = new Message(incoming);
-
-        TwiMLResponse twiml = new TwiMLResponse();
-        try {
-            twiml.append(message);
-        } catch (TwiMLException e) {
-            e.printStackTrace();
-        }
-
-        response.setContentType("application/xml");
-        response.getWriter().print(twiml.toXML());
-
-        */
     }
 }

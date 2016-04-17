e33d55021f37879c45c8099c248b8f92acef076b
Jaya Kasa <kasa288@gmail.com>
2016-04-17 00:06:55 -0400

    modified web.xml

--- src/test/java/twiliotestcode/Test.java
+++ src/test/java/twiliotestcode/Test.java
@@ -10,23 +10,33 @@ import com.twilio.sdk.resource.instance.*;
 import com.twilio.sdk.resource.list.*;
 import org.apache.http.NameValuePair;
 import org.apache.http.message.BasicNameValuePair;
+import com.twilio.sdk.TwilioRestException;
+
 
 public class Test {
     // Find your Account Sid and Token at twilio.com/user/account
-    public static final String ACCOUNT_SID = "AC33c9de5b0af9acdfdb7e69e33ee7c3ac";
-    public static final String AUTH_TOKEN = "c3da874c9c127dcc1c6d6c65ea45616b";
+    private static final String ACCOUNT_SID = "AC33c9de5b0af9acdfdb7e69e33ee7c3ac";
+    private static final String AUTH_TOKEN = "c3da874c9c127dcc1c6d6c65ea45616b";
+
+    //tests that the exception works
+    //private static final String ACCOUNT_SID = "ACsdaxosdaxosdaxosdaxosdaxosdaxosd";
+    //private static final String AUTH_TOKEN = "c3da874c9c127dcc1c6d6c65ea45616b";
 
     public static void main(String[]args) throws TwilioRestException {
-        TwilioRestClient client = new TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN);
+        try{
+            TwilioRestClient client = new TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN);
 
-        // Build the parameters
-        List<NameValuePair> params = new ArrayList<NameValuePair>();
-        params.add(new BasicNameValuePair("To", "7327630159"));
-        params.add(new BasicNameValuePair("From", "+13236854799"));
-        params.add(new BasicNameValuePair("Body", "TESTING"));
+            // Build the parameters
+            List<NameValuePair> params = new ArrayList<NameValuePair>();
+            params.add(new BasicNameValuePair("To", "7327630159"));
+            params.add(new BasicNameValuePair("From", "+13236854799"));
+            params.add(new BasicNameValuePair("Body", "TESTING"));
 
-        MessageFactory messageFactory = client.getAccount().getMessageFactory();
-        Message message = messageFactory.create(params);
-        //System.out.println(message.getSid());
+            MessageFactory messageFactory = client.getAccount().getMessageFactory();
+            Message message = messageFactory.create(params);
+            //System.out.println(message.getSid());
+        }catch(TwilioRestException e){
+            System.out.println("ERROR\n" + e.getErrorMessage());
+        }
     }
 }

f29736b4890b83473245f3ca5cc40424eb7875b1
Jaya Kasa <kasa288@gmail.com>
2016-04-17 02:21:11 -0400

    hooked up ReginaConnect to TwillioServletResponse

--- src/Controller/TwillioServletResponse.java
+++ src/Controller/TwillioServletResponse.java
@@ -25,6 +25,10 @@ public class TwillioServletResponse extends HttpServlet {
         String fromNumber = request.getParameter("From");
         String body = request.getParameter("Body");
 
+        System.out.println("SessionID " + sessionId);
+        System.out.println("fromNumber " + fromNumber);
+        System.out.println("body " + body);
+
         String incoming = ReginaConnect.getInstance().getRequest(body,fromNumber);
 
         Message message = new Message(incoming);

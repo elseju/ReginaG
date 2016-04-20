dfde446d39bf56efd08ab0033314dbc14b6563ad
User <user@Users-Mac-mini.home>
2016-04-19 23:06:51 -0400

    clean up

--- src/Controller/twillioServlet.java
+++ src/Controller/twillioServlet.java
@@ -12,16 +12,16 @@ import java.util.List;
 public class twillioServlet {
 
 	 // Find your Account Sid and Token at twilio.com/user/account
-    public static final String ACCOUNT_SID = "AC8abf2b3598450d06e001142ea2512484";
-    public static final String AUTH_TOKEN = "f5b91031ff8109a0535ae72d6ee737e4";
+    public static final String ACCOUNT_SID = "";
+    public static final String AUTH_TOKEN = "";
 
     public void sendMessage() throws TwilioRestException {
         TwilioRestClient client = new TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN);
 
         // Build the parameters
         List<NameValuePair> params = new ArrayList<NameValuePair>();
-        params.add(new BasicNameValuePair("To", "5595732440"));
-        params.add(new BasicNameValuePair("From", "+16674015610"));
+        params.add(new BasicNameValuePair("To", ""));
+        params.add(new BasicNameValuePair("From", ""));
         params.add(new BasicNameValuePair("Body", "TESTING"));
 
         MessageFactory messageFactory = client.getAccount().getMessageFactory();
